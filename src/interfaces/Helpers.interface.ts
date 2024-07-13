244a407acb6d843e24757b75d7a23c65dd091de4
nicolasguenin <nicolasguenin@protonmail.com>
2024-07-13 21:05:48 +0200

    refactor: remove unnecessary code

--- src/interfaces/Helpers.interface.ts
+++
@@ -1,5 +0,0 @@
-import { JSX } from 'react';
-
-export type ChildrenProps = {
-  children: string | JSX.Element | JSX.Element[] | (() => JSX.Element);
-};
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,1 @@
-export * from './Helpers.interface';
 export * from './Colors.interface';
